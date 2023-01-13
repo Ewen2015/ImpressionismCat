e05edf32e58951c39e6805b716b96029a4f48824
Ewen Wang <wang.enqun@outlook.com>
2023-01-13 11:14:12 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ImpressionismCat'
-copyright = '2022, Ewen Wang'
+copyright = '2023, Ewen Wang'
 author = 'Ewen Wang'
-release = '0.0.1'
+release = '0.0.x'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
